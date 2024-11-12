227e18f98377955fa44f7a9966cf2f2fdf17dfc6
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-12 14:26:51 -0500

    Revert "mongo_url"
    This reverts commit 18783467a51c9bb379df5441e5a5026be5ec869b.

--- server/app.ts
+++ server/app.ts
@@ -18,7 +18,7 @@ import loginController from './controller/login';
 
 dotenv.config();
 
-const MONGO_URL = `${process.env.MONGODB_URI || 'mongodb+srv://descoteauxa:03ssG0OcLftf31wo@db-cs4530-f24-508.pzdc8.mongodb.net'}/fake_so`;
+const MONGO_URL = `${process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017'}/fake_so`;
 const CLIENT_URL = process.env.CLIENT_URL || 'https://cs4530-f24-508.onrender.com';
 const port = parseInt(process.env.PORT || '8000');
 
