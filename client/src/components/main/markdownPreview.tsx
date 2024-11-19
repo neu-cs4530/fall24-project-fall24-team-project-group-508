15af4fa698d42e5eddb6b634e928af67a94ed9fc
adescoteaux1 <ally.descoteaux.0@gmail.com>
2024-11-19 00:17:36 -0500

    trying to get dependency :(

--- client/src/components/main/markdownPreview.tsx
+++ client/src/components/main/markdownPreview.tsx
@@ -1,10 +1,25 @@
+// import ReactMarkdown from 'react-markdown';
+// import remarkMath from 'remark-math';
+// import rehypeKatex from 'rehype-katex';
+// import 'katex/dist/katex.min.css';
+
+// const MarkdownPreview = ({ text }: { text: string }) => (
+//   <div className='markdown_preview_container'>
+//     <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
+//       {text}
+//     </ReactMarkdown>
+//   </div>
+// );
+
+// export default MarkdownPreview;
+
 import ReactMarkdown from 'react-markdown';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import 'katex/dist/katex.min.css';
 
 const MarkdownPreview = ({ text }: { text: string }) => (
-  <div className='markdown_preview_container'>
+  <div className="markdown_preview_container">
     <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
       {text}
     </ReactMarkdown>
