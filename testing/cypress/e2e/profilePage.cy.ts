65fd491eecd574271ae66ba58e61b69660c08570
chloeylee <chloester11204@gmail.com>
2024-12-01 02:05:28 -0600

    profile page test added

---
+++ testing/cypress/e2e/profilePage.cy.ts
@@ -0,0 +1,36 @@
+describe('ProfilePage', () => {
+    beforeEach(() => {
+        // Visit the page containing the AccessibilityPopup
+        cy.visit('https://cs4530-f24-508.onrender.com/');
+
+        // login to acc
+        cy.contains('Login').should('be.visible');
+        cy.get('input[name="username"]').type('test111');
+        cy.get('input[name="password"]').type('test');
+        cy.contains('Login').click();
+
+        cy.get('[data-testid="open-profile-popup-btn"]').click();
+    });
+
+    it('should render the profile picture', () => {
+        cy.get('svg[data-testid="PersonOutlineIcon"]').should('be.visible');
+    });
+
+    it('should display the user name and score', () => {
+        cy.get('label').contains('name:').should('exist');
+        cy.get('label').contains('score:').should('exist');
+
+        cy.get('label').contains('name: testUser').should('exist');
+        cy.get('label').contains('score: 0').should('exist');
+    });
+
+    it('should apply correct styles based on the theme', () => {
+        cy.get('div.profile-page')
+            .should('have.css', 'background-color')
+            .and('eq', 'rgb(255, 255, 255)');
+
+        cy.get('div.profile-page')
+            .should('have.css', 'color')
+            .and('eq', 'rgb(0, 0, 0)');
+    });
+});
